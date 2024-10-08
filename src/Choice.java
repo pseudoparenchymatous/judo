8d0ec0d418f715c0e780a386eeee5ea204b2e418
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-10-08 14:10:46 +0000

    `Choice`: set printing and getting choices as default methods

--- src/Choice.java
+++ src/Choice.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public interface Choice {
-    int getUserChoice(String[] choices);
+    default int getUserChoice(String[] choices) {
+        printChoices(choices);
+        int choicesLen = choices.length;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("\u001B[32m");
+        int userInput = 0;
+        do {
+            if (input.hasNextInt()) {
+                userInput = input.nextInt();
+            } else {
+                input.next();
+            }
+            System.out.print("\u001B[0m");
+
+            if (userInput < 1 || userInput > choicesLen) {
+                System.out.println("\u001b[31mInvalid input.\u001b[0m Input 1-" + choicesLen);
+            }
+        } while (userInput < 1 || userInput > choicesLen);
+        return userInput;
+    }
+
+    default void printChoices(String[] choices) {
+        for (int i = 0; i < choices.length; i++) {
+            System.out.printf("%d.) %s\n", i + 1, choices[i]);
+        }
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -4,29 +4,6 @@ public class Menu implements Choice {
     private final String[] welcomeMessages = {"Welcome to North Sussex Judo", "What would you like to do?"};
     private final String[] choices = {"Register athlete", "Print costs", "Exit"};
 
-    @Override
-    public int getUserChoice(String[] choices) {
-        printChoices(choices);
-        int choicesLen = choices.length;
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("\u001B[32m");
-        int userInput = 0;
-        do {
-            if (input.hasNextInt()) {
-                userInput = input.nextInt();
-            } else {
-                input.next();
-            }
-            System.out.print("\u001B[0m");
-
-            if (userInput < 1 || userInput > choicesLen) {
-                System.out.println("\u001b[31mInvalid input.\u001b[0m Input 1-" + choicesLen);
-            }
-        } while (userInput < 1 || userInput > choicesLen);
-        return userInput;
-    }
-
     public Menu() {
         AthleteInput athleteInput = new AthleteInput();
         AthleteOutput athleteOutput = new AthleteOutput();
@@ -52,12 +29,6 @@ public class Menu implements Choice {
     private void printWelcomeMessages() {
         for (String message: welcomeMessages) {
             System.out.println(message);
-        }
-    }
-
-    private void printChoices(String[] choices) {
-        for (int i = 0; i < choices.length; i++) {
-            System.out.printf("%d.) %s\n", i + 1, choices[i]);
         }
     }
 }
