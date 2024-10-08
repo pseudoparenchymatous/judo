c2f9a5633cbf8358fc4327de374f30a752fc7629
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-10-08 09:24:33 +0800

    `Menu`: handle user input and print process based on input

--- src/Menu.java
+++ src/Menu.java
@@ -4,10 +4,20 @@ public class Menu {
     private final String[] welcomeMessages = {"Welcome to North Sussex Judo", "What would you like to do?"};
     private final String[] options = {"Register athlete", "Print costs"};
 
-
     public Menu() {
         printWelcomeMessages();
         printOptions();
+        int userInput = getUserInput(options.length);
+        switch (userInput) {
+            case 1:
+                System.out.println("Adding athlete");
+                break;
+            case 2:
+                System.out.println("Printing costs");
+                break;
+            default:
+                System.out.println("Unexpected input");
+        }
     }
 
     private void printWelcomeMessages() {
@@ -21,4 +31,22 @@ public class Menu {
             System.out.printf("%d.) %s\n", i + 1, options[i]);
         }
     }
+
+    private int getUserInput(int noOfChoices) {
+        Scanner input = new Scanner(System.in);
+        int userInput = 0;
+        do {
+            if (input.hasNextInt()) {
+                userInput = input.nextInt();
+            } else {
+                input.next();
+            }
+
+            if (userInput < 1 || userInput > noOfChoices) {
+                System.out.println("Invalid input. Input 1-" + noOfChoices);
+            }
+        } while (userInput < 1 || userInput > noOfChoices);
+        return userInput;
+    }
+
 }
