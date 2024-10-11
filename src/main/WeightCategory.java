c106cc5a16e8d794c47d612658e986fb4a460392
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-10-11 10:39:33 +0000

    feat(`WeightCategory`): return better `String` representations

--- src/main/WeightCategory.java
+++ src/main/WeightCategory.java
@@ -1,17 +1,47 @@
 package main;
 
 public enum WeightCategory {
-    FLYWEIGHT,
-    LIGHTWEIGHT,
-    LIGHTMIDDLEWEIGHT,
-    MIDDLEWEIGHT,
-    LIGHTHEAVYWEIGHT,
-    HEAVYWEIGHT;
+    FLYWEIGHT {
+        @Override
+        public String toString() {
+            return "Flyweight";
+        }
+    },
+    LIGHTWEIGHT {
+        @Override
+        public String toString() {
+            return "Lightweight";
+        }
+    },
+    LIGHTMIDDLEWEIGHT {
+        @Override
+        public String toString() {
+            return "Light-Middleweight";
+        }
+    },
+    MIDDLEWEIGHT {
+        @Override
+        public String toString() {
+            return "Middleweight";
+        }
+    },
+    LIGHTHEAVYWEIGHT {
+        @Override
+        public String toString() {
+            return "Light-Heavyweight";
+        }
+    },
+    HEAVYWEIGHT {
+        @Override
+        public String toString() {
+            return "Heavyweight";
+        }
+    };
 
     public static String[] getCategories() {
         String[] categories = new String[WeightCategory.values().length];
         for (int i = 0; i < categories.length; i++) {
-            categories[i] = WeightCategory.values()[i].name();
+            categories[i] = WeightCategory.values()[i].toString();
         }
         return categories;
     }
