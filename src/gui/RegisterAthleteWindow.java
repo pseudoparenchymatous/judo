b61302e73fbcd99a3dcad4cf2ac1e8d36bbec841
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-10-15 18:10:50 +0800

    Finish `RegisterAthleteWindow`

--- src/gui/RegisterAthleteWindow.java
+++ src/gui/RegisterAthleteWindow.java
@@ -1,18 +1,43 @@
 package gui;
 
+import main.TrainingPlan;
+import main.WeightCategory;
+
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 
 public class RegisterAthleteWindow implements OptionWindow {
     private JFrame frame;
     private JPanel framePanel;
 
+    private JLabel invalidNameLabel = new JLabel("Invalid value. Must only include letters");
+
+    private JTextField nameField;
+    private JSpinner weightSpinner;
+    private JComboBox<WeightCategory> weightCategoryJComboBox;
+    private JSpinner privateCoachingSpinner;
+
+    private JComboBox<TrainingPlan> trainingPlanJComboBox;
+    private JRadioButton oneCompetitionRadioButton;
+    private JRadioButton zeroCompetitionRadioButton;
+    private ButtonGroup competitionsButtonGroup;
+
+    private JButton submitButton;
+
     public RegisterAthleteWindow() {
-        frame = new JFrame();
+        frame = new JFrame("Enter athlete details");
         framePanel = new JPanel();
         framePanel.setLayout(new BoxLayout(framePanel, BoxLayout.Y_AXIS));
 
         framePanel.add(getNamePanel());
+        framePanel.add(getTrainingPlanPanel());
+        framePanel.add(getWeightPanel());
+        framePanel.add(getCategoryPanel());
+        framePanel.add(getCompetitionsPanel());
+        framePanel.add(getPrivateCoachingPanel());
+        framePanel.add(getButtonsPanel());
 
         frame.add(framePanel);
     }
@@ -29,14 +54,178 @@ public class RegisterAthleteWindow implements OptionWindow {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
-        JLabel nameLabel = new JLabel("Name: ");
-        JTextField field = new JTextField();
+        JLabel nameLabel = new JLabel("Name");
+        nameField = new JTextField();
         Dimension dimension = new Dimension(120, 20);
-        field.setMaximumSize(dimension);
+        nameField.setPreferredSize(dimension);
+        nameField.setMinimumSize(dimension);
+        nameField.setMaximumSize(dimension);
+
+        nameField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                checkName(nameField);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                checkName(nameField);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+            }
+        });
+
+        invalidNameLabel.setForeground(Color.RED);
+        invalidNameLabel.setVisible(false);
 
         panel.add(nameLabel);
-        panel.add(field);
+        panel.add(getInvisibleGap(5));
+        panel.add(nameField);
+        panel.add(invalidNameLabel);
 
         return panel;
+    }
+
+    private JPanel getTrainingPlanPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+        JLabel label = new JLabel("Training plan");
+        trainingPlanJComboBox = new JComboBox<>(TrainingPlan.values());
+
+        panel.add(label);
+        panel.add(getInvisibleGap(5));
+        panel.add(trainingPlanJComboBox);
+
+        trainingPlanJComboBox.addActionListener(e -> checkTrainingPlan());
+
+        return panel;
+    }
+
+    private JPanel getWeightPanel() {
+        JPanel panel = new JPanel();
+
+        JLabel label = new JLabel("Weight");
+
+        SpinnerNumberModel weightModel = new SpinnerNumberModel(40.00, 40.00, 200.00, 0.1);
+        weightSpinner = new JSpinner(weightModel);
+
+        panel.add(label);
+        panel.add(getInvisibleGap(5));
+        panel.add(weightSpinner);
+
+        return panel;
+    }
+
+    private JPanel getCategoryPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+        JLabel label = new JLabel("Weight category");
+         weightCategoryJComboBox = new JComboBox<>(WeightCategory.values());
+
+        panel.add(label);
+        panel.add(getInvisibleGap(5));
+        panel.add(weightCategoryJComboBox);
+
+        return panel;
+    }
+
+    private JPanel getCompetitionsPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+        JLabel label = new JLabel("Competitions");
+
+        competitionsButtonGroup = new ButtonGroup();
+        zeroCompetitionRadioButton = new JRadioButton("0");
+        oneCompetitionRadioButton = new JRadioButton("1");
+        competitionsButtonGroup.add(zeroCompetitionRadioButton);
+        competitionsButtonGroup.add(oneCompetitionRadioButton);
+
+        zeroCompetitionRadioButton.setSelected(true);
+        oneCompetitionRadioButton.setEnabled(false);
+
+        panel.add(label);
+        panel.add(zeroCompetitionRadioButton);
+        panel.add(oneCompetitionRadioButton);
+
+        return panel;
+    }
+
+    private JPanel getPrivateCoachingPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+        JLabel label = new JLabel("Private coaching hours");
+        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(0, 0, 5, 1);
+        privateCoachingSpinner = new JSpinner(spinnerModel);
+
+        panel.add(label);
+        panel.add(getInvisibleGap(5));
+        panel.add(privateCoachingSpinner);
+
+        return panel;
+    }
+
+    private JPanel getButtonsPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
+        JButton resetButton = new JButton("Reset");
+        submitButton = new JButton("Submit");
+
+        submitButton.setEnabled(false);
+
+        resetButton.addActionListener(e -> resetForm());
+
+        panel.add(resetButton);
+        panel.add(submitButton);
+
+        return panel;
+    }
+
+    private void checkName(JTextField field) {
+        boolean validName = true;
+        String name = field.getText();
+        for (char c: name.toCharArray()) {
+           if (!Character.isSpaceChar(c)) {
+               if (!Character.isLetter(c)) {
+                   validName = false;
+               }
+           }
+        }
+
+        if (validName || name.isEmpty()) {
+            invalidNameLabel.setVisible(false);
+            submitButton.setEnabled(true);
+        } else {
+            invalidNameLabel.setVisible(true);
+            submitButton.setEnabled(false);
+        }
+
+    }
+
+    private void checkTrainingPlan() {
+        if (trainingPlanJComboBox.getSelectedItem() == TrainingPlan.BEGINNER) {
+            zeroCompetitionRadioButton.setSelected(true);
+            oneCompetitionRadioButton.setEnabled(false);
+        } else {
+            oneCompetitionRadioButton.setEnabled(true);
+        }
+    }
+
+    private void resetForm() {
+        nameField.setText("");
+        trainingPlanJComboBox.setSelectedIndex(0);
+        weightSpinner.setValue(40.00);
+        weightCategoryJComboBox.setSelectedIndex(0);
+        privateCoachingSpinner.setValue(0);
+    }
+
+    private Component getInvisibleGap(int gap) {
+        return Box.createHorizontalStrut(gap);
     }
 }
