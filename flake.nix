f3689fd87fb11601d2cd280e7666dfc18e4336c1
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-10-16 14:47:38 +0000

    flake: build/run flake with nix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
+    javac = "${pkgs.jdk}/bin/javac";
   in {
     devShells.${system}.default = pkgs.mkShell {
       name = "java-devshell";
@@ -24,6 +26,30 @@
       '';
 
       # Environment variables
+    };
+
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "nsj";
+        version = "1.0";
+        src = ./src;
+
+        buildPhase = ''
+          rm -rf target
+          ${javac} -Xlint:none -d target -sourcepath . main/Main.java
+        '';
+
+        installPhase = ''
+          mkdir -p $out/target
+          mv target $out/
+
+          mkdir $out/bin
+          cat > $out/bin/nsj << EOF
+          #!${pkgs.bash}/bin/bash
+          ${pkgs.jdk}/bin/java -cp $out/target main.Main
+          EOF
+
+          chmod +x $out/bin/nsj
+        '';
     };
   };
 }
