25b1f3080ec290633da455d938db644b9d1350e9
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-10-21 00:19:46 +0800

    `AthleteList`: add 6 current athletes

--- src/main/AthleteList.java
+++ src/main/AthleteList.java
@@ -4,10 +4,52 @@ import java.util.ArrayList;
 
 public class AthleteList {
     private static AthleteList instance;
-    private ArrayList<Athlete> list;
+    private final ArrayList<Athlete> list;
 
     private AthleteList() {
         list = new ArrayList<>();
+        list.add(new Athlete(
+                "Alex",
+                TrainingPlan.INTERMEDIATE,
+                98.5,
+                WeightCategory.LIGHT_HEAVYWEIGHT,
+                1,
+                5));
+        list.add(new Athlete(
+                "Bob",
+                TrainingPlan.ELITE,
+                110.6,
+                WeightCategory.HEAVYWEIGHT,
+                1,
+                3));
+        list.add(new Athlete(
+                "Charlie",
+                TrainingPlan.ELITE,
+                96.4,
+                WeightCategory.LIGHT_HEAVYWEIGHT,
+                0,
+                0));
+        list.add(new Athlete(
+                "Danny",
+                TrainingPlan.BEGINNER,
+                60.5,
+                WeightCategory.FLYWEIGHT,
+                0,
+                0));
+        list.add(new Athlete(
+                "Edward",
+                TrainingPlan.ELITE,
+                105.7,
+                WeightCategory.HEAVYWEIGHT,
+                1,
+                1));
+        list.add(new Athlete(
+                "Frank",
+                TrainingPlan.INTERMEDIATE,
+                86.2,
+                WeightCategory.MIDDLEWEIGHT,
+                1,
+                1));
     }
 
     public static AthleteList getList() {
@@ -32,5 +74,13 @@ public class AthleteList {
         }
 
         return athlete;
+    }
+
+    public String[] getNames() {
+        String[] names = new String[list.size()];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = list.get(i).getName();
+        }
+        return names;
     }
 }
